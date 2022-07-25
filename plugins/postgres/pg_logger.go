f63c9170982aaf9ddd70d718e3bc8f10cab13e28
Andes Lam <andes@shoplineapp.com>
2022-07-25 15:49:07 +0800

    chore(postgres): move calculate-api postgres to go-app

--- plugins/postgres/pg_logger.go
+++ plugins/postgres/pg_logger.go
@@ -2,17 +2,19 @@ package postgres
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/shoplineapp/go-app/plugins/logger"
 	"github.com/uptrace/bun"
 )
 
-type PgLogger struct{}
+type PgLogger struct {
+	logger *logger.Logger
+}
 
 func (d PgLogger) BeforeQuery(c context.Context, q *bun.QueryEvent) context.Context {
 	return c
 }
 
 func (d PgLogger) AfterQuery(c context.Context, q *bun.QueryEvent) {
-	fmt.Println(q.Query)
+	d.logger.Debug(q.Query)
 }
